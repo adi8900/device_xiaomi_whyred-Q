e14c2ed62c20d289f3969e050a3791b11cddc81e
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 +0430

    whyred: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a

--- device.mk
+++ device.mk
@@ -408,6 +408,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Soong
+ PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/whyred
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
